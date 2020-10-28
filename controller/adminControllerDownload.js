b9f219bd46e5a45e896cd22c1370de7d8eea8093
proyectosMarslnlau <ingmarcelopoma@gmail.com>
2020-10-28 12:25:16 -0300

    qza

--- controller/adminControllerDownload.js
+++ controller/adminControllerDownload.js
@@ -260,11 +260,7 @@ exports.documentoPDF = async(req, res) => {
             const content = `
                 <!doctype html>
                 <html>
-                <!-- Compiled and minified CSS -->
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-            
-                <!-- Compiled and minified JavaScript -->
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+                
                 <head>
                     <meta charset="utf-8">
                     <title>PDF Result Template</title>
